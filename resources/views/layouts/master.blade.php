fbbc6c4864f5874dc5eb857e6285de97268ee363
PreciousDiaz <elladiaz5@gmail.com>
2017-01-03 01:07:43 +0800

    chapter 3 : modify master.blade

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,11 +3,12 @@
     <head>
      
         <title>@yield('title')</title>
-
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
      
     </head>
     <body>
 	<div>
+	@include('includes.header')
        @yield('content')
 		</div>
 	
