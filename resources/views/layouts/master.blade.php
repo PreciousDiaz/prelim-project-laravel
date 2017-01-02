df996c24cc170bbb2f38c9cccea7ba3b297654c5
PreciousDiaz <elladiaz5@gmail.com>
2017-01-02 22:59:37 +0800

    chapter 1: create layouts folder and master.blade.php file

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+     
+        <title>@yield('title')</title>
+
+     
+    </head>
+    <body>
+	<div>
+       @yield('content')
+		</div>
+	
+	</body>
+</html>
